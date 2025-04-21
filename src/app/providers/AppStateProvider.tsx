a2aa083716d12125657ce32a77cb70f543a1c750
Nago730 <jqk1797@gmail.com>
2025-04-21 23:22:06 +0900

    refactor: DateContext 삭제제

--- src/app/providers/AppStateProvider.tsx
+++ src/app/providers/AppStateProvider.tsx
@@ -1,13 +1,10 @@
 import { ReactNode } from 'react';
-import { DateProvider } from '@/shared/contexts/DateContext';
 import { ModalProvider } from '@/shared/contexts/ModalContext';
 
 export const AppStateProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <DateProvider>
       <ModalProvider>
         {children}
       </ModalProvider>
-    </DateProvider>
   );
 }; 
