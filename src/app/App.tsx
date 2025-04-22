ba3b8546356b3119ea1d039056178572ebba5977
Nago730 <jqk1797@gmail.com>
2025-04-22 16:49:37 +0900

    refactor: default export → named export 방식으로 변경

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Header from '@/widgets/Header';
+import { Header } from '@/widgets/Header';
 import { AppStateProvider } from './providers/AppStateProvider';
 
 const App = () => (
